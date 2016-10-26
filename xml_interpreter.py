204f88d9d0d3700ca4ece8fc1621e4b7889b2496
Bennett Hensey <bennett.hensey@gmail.com>
2016-10-26 16:39:06 -0500

    Added command line args and basic language support

--- xml_interpreter.py
+++ xml_interpreter.py
@@ -1,29 +1,35 @@
-import mwxml, re, pandas as pd
+import mwxml, re, pandas as pd, argparse
+from lang_dict import lang_dict
 
 df=pd.DataFrame(columns=['lang','page_id','page_title','subheading_title','user_text','indentation_depth','subheading_line'])
 
 # All necessary re-patterns
 NEW_LINE=r'\n\n|\n'
 MISC=r'(\s*(\[\[[^\]]*]])*\s*.){0,10}'  #matches either a space, a page element (anything between brackets), or any character, up to 10 times
-SUBHEAD=r'(?<!=)==[^=].*?==';
+SUBHEAD=r'(?<!=)==[^=].*?=='
 
-COMMENT_REG=r'\[\[[Uu]ser[^|\]]{1,55}\|[^|]+]]'
-COMMENT_TALK=COMMENT_REG+MISC+r'\[\[[Uu]ser[^|\]]+:[^|]+\|[^\]]+]]'   # Talk page has a regular comment, some misc, then a talk comment
-COMMENT_ANON=r'\{\{subst:[^|\]]+\|[^}]+}}'
-ALL='|'.join([COMMENT_TALK,COMMENT_ANON,COMMENT_REG])
+# Construct argument parser for command line usage
+parser=argparse.ArgumentParser()
+parser.add_argument("-l","--lang",help="Define the language of the given wikipedia dump")
+parser.add_argument("-f","--file",help="Give path to Wikidump")
+parser.add_argument("-o","--output",help="Define output file location")
+args=parser.parse_args()
 
+if not (args.lang):
+     print('Error: must define --lang, --file, and --output')
+
+## Command line input test
+args.lang='en'
+
+# Create dictionary of regular expressions
 def define_comment_tags (lang_ed):
-     global COMMENT_REG, COMMENT_TALK, COMMENT_ANON
-     # get tag elements from csv
-#     lang_df=pd.read_csv('csvfile.csv')
-#     lang_df=lang_df[lang_df[lang==lang_ed]]
-#     user=lang_df[1]
-#     anonymous=lang_df[2]         
-     COMMENT_REG=r'\[\[User[^|\]]{1,55}\|[^|]+]]'
-     COMMENT_TALK=COMMENT_REG+MISC+r'\[\[User[^|\]]+:[^|]+\|[^\]]+]]'   # Talk page has a regular comment, some misc, then a talk comment
-     COMMENT_ANON=r'\{\{subst:[^|\]]+\|[^}]+}}'
+     user=lang_dict['user'][args.lang]
+     anonymous=lang_dict['user'][args.lang]
+     COMMENT_REG=r'\[\['+user+'[^|\]]{1,55}\|[^|]+]]'
+     COMMENT_TALK=COMMENT_REG+MISC+r'\[\['+user+'[^|\]]+:[^|]+\|[^\]]+]]'   # Talk page has a regular comment, some misc, then a talk comment
+     COMMENT_ANON=r'\{\{'+anonymous+':+[^|\]]+\|[^}]+}}'
      ALL='|'.join([COMMENT_TALK,COMMENT_ANON,COMMENT_REG])
-     return    
+     return ALL
 
 
 # Define comment class
@@ -67,7 +73,7 @@ class comment(object):
 
 # Identify subheading title, find all comments by indentation and author tag, then loop through and
 # break them into comments
-def create_comments_list (subheading):
+def create_comments_list (subheading,ALL):
      sub_title=re.match(SUBHEAD, subheading)
      if sub_title:
           subheading=subheading[sub_title.end():]
@@ -79,6 +85,8 @@ def create_comments_list (subheading):
      line_begin=0
      #Grab last author from each line
      for line in newline_iterator:
+          # For individual line testing
+          #print(subheading[line_begin:line.start()]);input('pause')
           author=False
           for author in re.finditer(ALL,subheading[line_begin:line.end()]):
                pass
@@ -111,11 +119,11 @@ def split_subheadings_into_list (file):
      return subheading_list
 
 # Linearly runs through all the above functions
-def parse_page (revision,lang):
+def parse_page (revision,lang,ALL):
      file=revision.text
      subheading_list=split_subheadings_into_list(file)
      for subheading in subheading_list:
-          comments_list=create_comments_list(subheading)
+          comments_list=create_comments_list(subheading,ALL)
           comment_num=1
           for comment in comments_list:
                df.loc[len(df)]=(lang,revision.page.id,revision.page.title,comment.subheading_title,comment.username,comment.indent,comment_num)
@@ -123,11 +131,12 @@ def parse_page (revision,lang):
                # if speed is a concern a different method for writing data (probably to a csv) should be used
                #df=df.append(pandas.DataFrame(data=[[val1,val2,val3]],columns=['column name1', 'column name2']), ignore_index=True)
                comment_num+=1
-            
+              
 # Creates page iterator and feeds it into parse_page, appending the resulting dataframe to the master dataframe
 # skip empty talk pages
 def main():
      dump=mwxml.Dump.from_file(open(u'/Users/Bennett/Desktop/scraping/simplewiki-latest-pages-meta-current.xml','rb'))
+     ALL=define_comment_tags(args.lang)
      for page in dump:
          lang=dump.site_info.dbname
          if page.namespace==1:
@@ -135,9 +144,8 @@ def main():
                   if revision.text==None:
                        print('Empty: '+ revision.page.title)
                   else:
-                       parse_page(revision,lang)
-     df.to_csv('xml_output(current).csv',index=False)
+                       parse_page(revision,lang,ALL)
+     df.to_csv('xml_output(current).csv',index=False,na_rep='None')
 
 if __name__ =='__main__':
      main()
-
