4931d6f2c9b5c3a19654322d59132cf5f8fbfc17
Bennett Hensey <bennett.hensey@gmail.com>
2016-11-02 21:40:21 -0500

    Update xml_interpreter.py

--- xml_interpreter.py
+++ xml_interpreter.py
@@ -1,43 +1,50 @@
-import mwxml, re, pandas as pd, argparse
+import mwxml, re, pandas as pd, argparse, os
 from lang_dict import lang_dict
 
 df=pd.DataFrame(columns=['lang','page_id','page_title','subheading_title','user_text','indentation_depth','subheading_line'])
 
 # All necessary re-patterns
 NEW_LINE=r'\n\n|\n'
-MISC=r'(\s*(\[\[[^\]]*]])*\s*.){0,10}'  #matches either a space, a page element (anything between brackets), or any character, up to 10 times
 SUBHEAD=r'(?<!=)==[^=].*?=='
+IP_ADD=r'[0-9\.]{9,18}'
 
-# Construct argument parser for command line usage
-parser=argparse.ArgumentParser()
-parser.add_argument("-l","--lang",help="Define the language of the given wikipedia dump")
-parser.add_argument("-f","--file",help="Give path to Wikidump")
-parser.add_argument("-o","--output",help="Define output file location")
-args=parser.parse_args()
-
-if not (args.lang):
-     print('Error: must define --lang, --file, and --output')
-
-## Command line input test
-args.lang='en'
+anon_count=0
 
 # Create dictionary of regular expressions
-def define_comment_tags (lang_ed):
-     user=lang_dict['user'][args.lang]
-     anonymous=lang_dict['user'][args.lang]
-     COMMENT_REG=r'\[\['+user+'[^|\]]{1,55}\|[^|]+]]'
-     COMMENT_TALK=COMMENT_REG+MISC+r'\[\['+user+'[^|\]]+:[^|]+\|[^\]]+]]'   # Talk page has a regular comment, some misc, then a talk comment
-     COMMENT_ANON=r'\{\{'+anonymous+':+[^|\]]+\|[^}]+}}'
-     ALL='|'.join([COMMENT_TALK,COMMENT_ANON,COMMENT_REG])
+def define_comment_tags (lang):
+     user=lang_dict['user'][lang]
+     anon=lang_dict['anonymous'][lang][0]
+     user_rev=''.join(reversed(user))
+     anon_rev=''.join(reversed(anon))
+     user_list=[]
+     rev_user_list=[]
+     for user in lang_dict['user'][lang]:
+         user_list.append(user)
+         rev_user_list.append(''.join(reversed(user)))
+     user='|'.join(user_list)
+     rev_user='|'.join(rev_user_list)
+     anon_list=[]
+     rev_anon_list=[]
+     for anon in lang_dict['anonymous'][lang]:
+         anon_list.append(anon)
+         rev_anon_list.append(''.join(reversed(anon)))
+     anon='|'.join(anon_list)
+     rev_anon='|'.join(rev_anon_list)
+     COMMENT_REG=r'\|.{1,55}?:'+user_rev+r'\[\['
+     #COMMENT_REG=r'\|.{1,55}?:.{1,55}?\[\['
+     COMMENT_ANON=r'}}.{1,55}?\|('+rev_anon+'){{'
+     #COMMENT_ANON=r'}}.{1,55}\|.{1,55}{{'
+     
+     
+     ALL='|'.join([COMMENT_REG,COMMENT_ANON])
      return ALL
-
 
 # Define comment class
 class comment(object):
-     def __init__ (self,sub_title,comment_text,user_tag):
+     def __init__ (self,sub_title,comment_text,user_tag,lang):
           self.comment_text=comment_text
           self.subheading_title=self.subtitle_from_subtag(sub_title)
-          self.username=self.username_from_usertag(user_tag)
+          self.username=self.username_from_usertag(user_tag,lang)
           self.indent=self.comment_indents(comment_text)
           
      # Find indentation by finding the number of starting colons, excluding certain characters
@@ -51,17 +58,21 @@ class comment(object):
           return indent
      
      # Find user name in user tag
-     def username_from_usertag (self,usertag):
+     def username_from_usertag (self,usertag,lang):
+         global anon_count
+         user=lang_dict['user'][lang]
          if usertag==None:
               username=None
-         elif usertag.startswith('[[User:'):
-             try:
-                  username=re.match(r'[^|]+',usertag[7:]).group()
-             except:
-                  print('re-match didnt work: '+usertag)
-                  username=None
+         elif re.match(r'\[\['+user+':',usertag,re.IGNORECASE):
+             username=re.match(r'[^|]+',usertag[3+len(user):]).group()
+         elif re.match(r'{{.{1,55}?\|',usertag):
+             username=usertag[re.match(r'{{.{1,55}?\|',usertag).end():-2];
+             #print(username);
+             #print(usertag);
+             #input('pause');
          else:
-             username='ANONYMOUS'
+             username='UNDEFINED'
+             #print(usertag);input('pause')
          return username
 
      def subtitle_from_subtag (self,subtag):
@@ -73,38 +84,38 @@ class comment(object):
 
 # Identify subheading title, find all comments by indentation and author tag, then loop through and
 # break them into comments
-def create_comments_list (subheading,ALL):
+def create_comments_list (subheading,lang,ALL):
      sub_title=re.match(SUBHEAD, subheading)
      if sub_title:
           subheading=subheading[sub_title.end():]
-          sub_title=sub_title.group()
+          sub_title=sub_title.group()  
      else:
           sub_title='Top_Subtitle'
      comments_list=list()
+     last_end=0
      newline_iterator=re.finditer(NEW_LINE, subheading)
-     line_begin=0
      #Grab last author from each line
      for line in newline_iterator:
-          # For individual line testing
-          #print(subheading[line_begin:line.start()]);input('pause')
-          author=False
-          for author in re.finditer(ALL,subheading[line_begin:line.end()]):
-               pass
-          if author:
-               comment_text=subheading[line_begin:]
-               user_tag=subheading[line_begin+author.start():line_begin+author.end()]
-               comments_list.append(comment(sub_title,comment_text,user_tag))
-               line_begin=line.end()
+         #print(subheading[last_end:line.start()]);input('pause')
+         reversed_text=''.join(reversed(subheading[last_end:line.end()]))
+         if re.search(ALL,reversed_text):
+               tag_loc=re.search(ALL,reversed_text)
+               comment_text=subheading[last_end:line.start()]
+               user_tag=''.join(reversed(reversed_text[tag_loc.start():tag_loc.end()]))
+               comments_list.append(comment(sub_title,comment_text,user_tag,lang))
+         last_end=line.end()
      # Text does not end with newline, so check the end of the text after the loop
-     author=re.search(ALL,subheading[line_begin:])
-     if author:
-          comment_text=subheading[line_begin:]
-          user_tag=subheading[line_begin+author.start():line_begin+author.end()]
-          comments_list.append(comment(sub_title,comment_text,user_tag))
+     author=re.search(ALL,subheading[last_end:])
+     reversed_text=''.join(reversed(subheading[last_end:]))
+     if re.search(ALL,reversed_text):
+         tag_loc=re.search(ALL,reversed_text)
+         comment_text=''.join(reversed_text)
+         user_tag=''.join(reversed(reversed_text[tag_loc.start():tag_loc.end()]))
+         comments_list.append(comment(sub_title,comment_text,user_tag,lang))
      if comments_list==list():
           comment_text=''
           user_tag=None
-          comments_list.append(comment(sub_title,comment_text,user_tag))
+          comments_list.append(comment(sub_title,comment_text,user_tag,lang))
      return comments_list
                              
 # Find beginning of page, find all subheadings, then loop thorugh and break into a list
@@ -123,7 +134,7 @@ def parse_page (revision,lang,ALL):
      file=revision.text
      subheading_list=split_subheadings_into_list(file)
      for subheading in subheading_list:
-          comments_list=create_comments_list(subheading,ALL)
+          comments_list=create_comments_list(subheading,lang,ALL)
           comment_num=1
           for comment in comments_list:
                df.loc[len(df)]=(lang,revision.page.id,revision.page.title,comment.subheading_title,comment.username,comment.indent,comment_num)
@@ -134,18 +145,44 @@ def parse_page (revision,lang,ALL):
               
 # Creates page iterator and feeds it into parse_page, appending the resulting dataframe to the master dataframe
 # skip empty talk pages
-def main():
-     dump=mwxml.Dump.from_file(open(u'/Users/Bennett/Desktop/scraping/simplewiki-latest-pages-meta-current.xml','rb'))
-     ALL=define_comment_tags(args.lang)
+def parse_dump(lang):
+     
+     # Get dump file
+     cwd = os.getcwd()
+     if os.path.isfile(cwd+'/wiki_dumps/'+lang+'wiki-latest-pages-meta-current.xml'):
+          file_path = cwd+u'/wiki_dumps/'+lang+'wiki-latest-pages-meta-current.xml'
+     else:
+          file_path = cwd+u'/wiki_dumps/'+lang+'wiki-latest-pages-meta-current1.xml'
+     dump=mwxml.Dump.from_file(open(file_path,'rb'))
+     
+     # Parse dump file
+     ALL=define_comment_tags(lang)
      for page in dump:
-         lang=dump.site_info.dbname
          if page.namespace==1:
              for revision in page:
                   if revision.text==None:
                        print('Empty: '+ revision.page.title)
                   else:
                        parse_page(revision,lang,ALL)
-     df.to_csv('xml_output(current).csv',index=False,na_rep='None')
+     df.to_csv('xml_output('+lang+').csv',index=False,na_rep='None',encoding="utf-8")
+
+
+# Construct argument parser for command line usage
+def main (input_lang):
+     parser=argparse.ArgumentParser()
+     parser.add_argument("-l","--lang",help="Define the language of the given wikipedia dump")
+     parser.add_argument("-f","--file",help="Give path to Wikidump")
+     parser.add_argument("-o","--output",help="Define output file location")
+     args=parser.parse_args()
+
+     # Enforce input requirement
+     if not (args.lang):
+          lang=input_lang
+     else:
+          lang=args.lang
+          
+     parse_dump(lang)
+     return
 
 if __name__ =='__main__':
-     main()
+     main('es')
