ca6161a2b066db9de0f33b6c4d1953d930ae6df7
Bennett Hensey <bennett.hensey@gmail.com>
2017-01-24 21:09:58 -0600

    Added overview/comments to class methods

--- bennett_code/server/xml_interpreter(8).py
+++ bennett_code/server/xml_interpreter(8).py
@@ -1,4 +1,5 @@
 import mwxml, re, pandas as pd, argparse, os, datetime
+# lang_dict is a python dictionary used to make different user tags for each language edition
 from lang_dict import lang_dict
 
 cwd = os.getcwd()
@@ -12,11 +13,13 @@ NEW_LINE=r'\n\n|\n'
 SUBHEAD=r'(?<!=)==[^=].*?=='
 
 # Test output utility
+# Used to get a csv output when the script is not allowed to run to completion
 def output_csv(name,lang):
      df.to_csv('data/raw_data/'+name+'_xml_output_'+lang+'.csv',index=False,na_rep='None',encoding='utf-8')
      return
 
-# Encoding for dataframe
+# Makes the text in the output csv the same as the text given by the wikipedia talk page json found online
+# (e.g. /u123456)
 def encode_text(text):
           try:
                text=text.encode('raw_unicode_escape').decode('utf-8')
@@ -24,7 +27,8 @@ def encode_text(text):
           except:
                return text
 
-# Create dictionary of regular expressions
+# Creates a single regular expression for a given language that catches any usertag defined in "lang_dict"
+# Different user tags are ordered in a relatively efficient way (most common tags are first)
 def define_comment_tags (lang):
      user_list=lang_dict['user'][lang]+lang_dict['user']['en']
      anon_list=lang_dict['anonymous'][lang]+lang_dict['anonymous']['en']
@@ -34,7 +38,7 @@ def define_comment_tags (lang):
      rev_anon=''.join(reversed('|'.join(reversed(anon_list))))
      COMMENT_REG=r'\|.{1,55}?:('+rev_user+r')\[\['
      COMMENT_ANON=r'}}.{1,55}?\|('+rev_anon+'){{'
-     # Find suitable text with square or curly brackets
+     # Uncomment this code to make a match with any tag-like text
      #COMMENT_REG=r'\|.{1,55}?:.{1,55}?\[\['
      #COMMENT_ANON=r'}}.{1,55}\|.{1,55}{{'
      
@@ -42,6 +46,8 @@ def define_comment_tags (lang):
      return ALL
 
 # Define comment class
+# The comment class takes the entire text of a comment (distinguished by a newline character) and uses methods to
+# extract the number of indents, username from usertag, and subheading from subtag
 class comment(object):
      def __init__ (self,sub_title,comment_text,user_tag,lang):
           self.comment_text=comment_text
@@ -59,7 +65,13 @@ class comment(object):
                comment_text=comment_text[1:]
           return indent
      
-     # Find user name in user tag
+     # Extract username from user tag.
+     # Can also be used to find new tags for input into "lang_dict". If the commented code in "define_comment_tags()" is 
+     # uncommented, all tag-like text is put through this method to try to find usertext. The bottom of this method can
+     # uncommented to print a message anytime a tag is found that is not accounted for in lang_dict. That tag text can
+     # then be added to lang_dict and the script will run again without triggering on that tag. This is done until
+     # all the tags that come up are distincly not usertags.
+    
      def username_from_usertag (self,usertag,lang):
          user_list=lang_dict['user'][lang]+lang_dict['user']['en']
          anon_list=lang_dict['anonymous'][lang]+lang_dict['anonymous']['en']
@@ -78,7 +90,9 @@ class comment(object):
          elif re.match(r'\{\{('+anon+')\|',usertag,re.I):
               line_pos=re.search('\|',usertag).end()
               username=usertag[line_pos:-2]
+              # Uncomment for languages with encoded text 
               #print('Encoded: '+usertag.encode('raw_unicode_escape').decode('utf-8'))
+              # Uncomment for languages with decoded text
               #print('Decoded: '+usertag)
               #print('Subtitle: '+self.subheading_title)
          # Use to catch unknown tags
@@ -93,6 +107,7 @@ class comment(object):
              input('')
          return encode_text(username)
 
+     # Extract subheading title from subtag
      def subtitle_from_subtag (self,subtag):
           if subtag=="Top_Subtitle":
                sub_title="Top_Subtitle"
